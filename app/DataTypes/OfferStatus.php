334f820dfb855d3079729aae144d93ad834778dc
GAZDA-EDU <gazda.edu@gmail.com>
2024-04-18 20:39:51 +0200

    tworzenie modeli przy uzyciu php artisan make:model + fabryka i migracja

---
+++ app/DataTypes/OfferStatus.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\DataTypes;
+
+abstract class OfferStatus {
+    const DRAFT = "Szkic";
+    const READY = "Gotowa";
+    const SENT = "Wysłana";
+
+    const TYPES = [
+        self::DRAFT,
+        self::READY,
+        self::SENT
+    ];
+}
