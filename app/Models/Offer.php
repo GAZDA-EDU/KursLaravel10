3c539522b8687c0bd248bde2aa64a213fd645bfc
GAZDA-EDU <gazda.edu@gmail.com>
2024-04-19 23:47:30 +0200

    Relacja wiele do wielu

--- app/Models/Offer.php
+++ app/Models/Offer.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Offer extends Model
 {
     use HasFactory;
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class, 'company_id');
+    }
+    public function products()
+    {
+        return $this->belongsToMany(Product::class, 'positions', 'offer_id', 'product_id');
+    }
 }
+
